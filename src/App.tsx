99cc99e0aea32c47d3f7dc735917c915fa741fe6
NVKGLOBAL <NVKGLOBAL@gmail.com>
2025-07-11 01:34:54 -0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EchoScribePanel from './components/EchoScribePanel';
+
+function App() {
+  return (
+    <div>
+      <h1>Codex Whisper Agent System Demo</h1>
+      <EchoScribePanel />
+    </div>
+  );
+}
+
+export default App;
